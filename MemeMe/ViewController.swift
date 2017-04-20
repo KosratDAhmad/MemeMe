4083a0e0af9c6d145e0cae661ff44793d4fd4895
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2017-04-20 14:17:01 +0300

    feat: Add image picker from garllery

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        
+        present(controller, animated: true, completion: nil)
+    }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("Picked an image \(info)")
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            imagePickerView.image = image
+        }
+        
+        dismissPicker()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Cancel image picker")
+        dismissPicker()
+    }
+    
+    func dismissPicker() {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
