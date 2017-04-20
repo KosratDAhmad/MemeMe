84674012cb67635d59ce84145d561c7973946639
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2017-04-20 15:21:51 +0300

    feat: Use camera to add an image to the app

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,25 +11,35 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
-
-    @IBAction func pickAnImage(_ sender: Any) {
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         
         let controller = UIImagePickerController()
         controller.delegate = self
+        controller.sourceType = .photoLibrary
         
         present(controller, animated: true, completion: nil)
     }
 
+    @IBAction func pickAnImageFromCamera(_ sender: Any){
+        
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .camera
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         print("Picked an image \(info)")
         
